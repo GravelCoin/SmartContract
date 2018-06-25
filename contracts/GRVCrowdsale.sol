dec47390d2a1a1b426d9c1937dcead4e337e3faf
Silvio Queiroz <silvio.queiroz@gmail.com>
2018-06-24 23:47:57 -0300

    Implements handle token sold

--- contracts/GRVCrowdsale.sol
+++ contracts/GRVCrowdsale.sol
@@ -3,13 +3,16 @@ pragma solidity ^0.4.23;
 import "../node_modules/zeppelin-solidity/contracts/math/SafeMath.sol";
 // Extension of Crowdsale contract whose tokens are minted in each purchase. Token ownership should be transferred to MintedCrowdsale for minting. 
 import "../node_modules/zeppelin-solidity/contracts/crowdsale/emission/MintedCrowdsale.sol";
-import "./BlockCrowdsale.sol";
+import "./IncreasingPriceCrowdsale.sol";
 
 /**
  * Crowdsale contract of the GravelCoin
  */
-contract GRVCrowdsale is MintedCrowdsale, BlockCrowdsale{
+contract GRVCrowdsale is MintedCrowdsale, IncreasingPriceCrowdsale{
     using SafeMath for uint256;
+    
+    /* the number of tokens already sold through this contract*/
+    uint256 public tokensSold = 0;
 
      /**
      * Construct of GRVCrowdsale.
@@ -19,7 +22,26 @@ contract GRVCrowdsale is MintedCrowdsale, BlockCrowdsale{
       Crowdsale(_rate, _wallet, _token)
       // TODO: implements rules of the release block and weidh value
       // FIXME: 
-      BlockCrowdsale(1, 1) {
+      IncreasingPriceCrowdsale(3, 10) {
     }
+
+  /**
+   * @dev Executed when a purchase has been validated and is ready to be executed. Not necessarily emits/sends tokens.
+   * @param _beneficiary Address receiving the tokens
+   * @param _tokenAmount Number of tokens to be purchased
+   *
+   * override Crowdsale._processPurchase
+   */
+    function _processPurchase(
+        address _beneficiary,
+        uint256 _tokenAmount
+    )
+      internal
+    {
+        // update token sold
+        tokensSold.add(_tokenAmount);
+        
+        super._deliverTokens(_beneficiary, _tokenAmount);
+    }    
 
 }
