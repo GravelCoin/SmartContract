3acab41f55a44679839590791a520f28971f5745
Silvio Queiroz <silvio.queiroz@gravelcoin.com>
2019-05-03 23:01:11 -0300

    Fix gasPrice

--- truffle.js
+++ truffle.js
@@ -11,7 +11,7 @@ module.exports = {
       port: 8545,
       network_id: "5777",
       gas: 5000000,
-      gasPrice: 10000000000
+      gasPrice: 2200000000
     },
     ropsten: {
       provider: function() {
@@ -19,7 +19,7 @@ module.exports = {
       },
       network_id: '3',
       gas: 5000000,
-      gasPrice: 10000000000
+      gasPrice: 2200000000
     },
     rinkeby: {
       provider: function() {
@@ -27,7 +27,7 @@ module.exports = {
       },
       network_id: '4',
       gas: 5000000,      //make sure this gas allocation isn't over 4M, which is the max      
-      gasPrice: 10000000000
+      gasPrice: 2200000000
     },
     kovan: {
       provider: function() {
@@ -35,7 +35,7 @@ module.exports = {
       },
       network_id: '5',
       gas: 5000000,      //make sure this gas allocation isn't over 4M, which is the max      
-      gasPrice: 10000000000
+      gasPrice: 2200000000
     },
     mainnet: {
       provider: function() {
@@ -43,7 +43,7 @@ module.exports = {
       },
       network_id: '1',
       gas: 5000000,      //make sure this gas allocation isn't over 4M, which is the max      
-      gasPrice: 10000000000
+      gasPrice: 2200000000
     }
   }
 };
